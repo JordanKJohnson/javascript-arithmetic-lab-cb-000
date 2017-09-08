620847e674f65e742d15bcc4e93d1c471af85388
Jordan Johnson <jkj86@hotmail.com>
2017-09-08 03:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function add(a,b){
   return a+b;
 }
+
+function subtract(a,b){
+  return a-b;
+}
+
+function multiply(a,b){
+  return a*b;
+}
+
+function divide(a,b){
+  return a/b;
+}
+
+function inc(a,b){
+  for (int i = 0; i < b; i++){
+    a*=a;
+  }
+  return a;
+}
